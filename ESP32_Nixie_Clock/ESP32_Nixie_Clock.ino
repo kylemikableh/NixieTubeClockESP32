a3f50197425b935a03e7e3c2e8796a6ec6022196
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2020-12-18 08:13:13 -0500

    Thread to check for NTP update every so often

--- ESP32_Nixie_Clock/ESP32_Nixie_Clock.ino
+++ ESP32_Nixie_Clock/ESP32_Nixie_Clock.ino
@@ -6,8 +6,12 @@ const char* ssid       = "Mikolajczyk";
 const char* password   = "mickeymouse";
 
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = -18001;
+const long  gmtOffset_sec = -18000;
 const int   daylightOffset_sec = 3600;
+
+pthread_t thread;
+int returnValue;
+int id = 1;
 
 void printLocalTime()
 {
@@ -17,28 +21,39 @@ void printLocalTime()
     return;
   }
   Serial.print(&timeinfo, "%A, %B %d %Y %H:%M:%S:");
+  struct timeval current_time;
+  gettimeofday(&current_time, NULL);
+  Serial.printf("%ld \n", current_time.tv_usec / 10000);
+}
+
+void getNTPUpdate() {
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\n\n\n\nCONNECTED\n\n\n\n");
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void *updateThread(void *threadid) {
+  while(true)
+  {
+    delay(10000);
+    getNTPUpdate();
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  
-  //connect to WiFi
-  Serial.printf("Connecting to %s ", ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println(" CONNECTED");
-  
-  //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.println("Hello World");
+  getNTPUpdate();
+  returnValue = pthread_create(&thread, NULL, updateThread, (void *)id);
   printLocalTime();
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
 }
 
 void loop()
